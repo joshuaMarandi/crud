62af595535b10d0db5490f5d8567ba035395219c
joshuaMarandi <134075135+joshuaMarandi@users.noreply.github.com>
2023-08-29 15:39:59 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,13 +4,12 @@ include 'connect.php';
 if(isset($_GET['deleteid']))
 {
     $id=$_GET['deleteid'];
-    
     $sql="delete from rekebisha where id='$id'";
     $result=mysqli_query($con,$sql);
 
     if ($result)
     {
-        echo"Deleted successful";
+    // echo"Deleted successful";
         
 header('location:display.php');
     }
