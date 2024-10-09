6d524b2e694e62bce85af1b73a06997cb732922c
Joshua Marandi <134075135+joshuaMarandi@users.noreply.github.com>
2024-10-09 14:05:53 +0300

    Update display.php

--- display.php
+++ display.php
@@ -28,7 +28,7 @@ include 'connect.php';
     <th scope="col">mobile</th>
     <th scope="col">password</th>
     <th scope="col">operations</th> 
-    <th scope="col">operations</th> 
+    <th scope=" 
 
 </tr>
 <?php
